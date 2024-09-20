f72d7fe76dfef6241b2558e16016aa17d074ab7a
Daniel Blancas <edblancas@gmail.com>
2024-09-20 16:52:02 -0600

    Clean up projects.py
    Only use projects.py
    Removed load and remote stuff
    Default to tabs when invoked

--- projects.py
+++ projects.py
@@ -1,7 +1,6 @@
 import argparse
 import json
 import os
-import re
 import subprocess
 from datetime import datetime
 from typing import List
@@ -20,58 +19,6 @@ parser.add_argument(
     help="directories to find projects",
 )
 
-parser.add_argument(
-    "--org",
-    dest="orgs",
-    action="append",
-    default=[],
-    help="look for repos in these github orgs",
-)
-
-parser.add_argument(
-    "--user",
-    dest="users",
-    action="append",
-    default=[],
-    help="look for repos for these github users",
-)
-
-
-def new_main(args, opts):
-    try:
-        url = input("🐈 New project\nenter name or github url: ")
-        return url
-    except KeyboardInterrupt:
-        return ""
-
-
-def new_handler(args: List[str], answer: str, target_window_id: int, boss: Boss):
-    opts = parser.parse_args(args[1:])
-
-    # This is the dir we clone repos into, for me it's not a big deal if they get cloned to the
-    # first dir. But some people might want to pick which dir to clone to? How could that be
-    # supported?
-    projects_root = opts.dirs[0]
-
-    if not answer:
-        return
-    elif "/" in answer:
-        # Note: This is an attempt to see if the answer is a git url or not, e.g.
-        #   - git@github.com:taylorzr/kitty-meow.git
-        #   - https://github.com/taylorzr/kitty-meow.git
-        github_url = answer
-        dir = re.split("[/.]", github_url)[2]
-        print(f"cloning into {dir}...")
-        path = f"{projects_root}/{dir}"
-        subprocess.run(["git", "clone", github_url, path])
-    else:
-        new_local = answer
-        dir = new_local
-        path = f"{projects_root}/{dir}"
-        os.makedirs(path, exist_ok=True)
-
-    load_project(boss, path, dir)
-
 
 def load_main(args, opts):
     # FIXME: How to call boss in the main function?
@@ -82,8 +29,8 @@ def load_main(args, opts):
         ).stdout.strip("\n")
     )
 
-    tabs = [tab["title"] for tab in kitty_ls[0]["tabs"]]
-    tabs_and_projects = [tab["title"] for tab in kitty_ls[0]["tabs"]]
+    tabs = [tab["title"] for tab in kitty_ls[0]["tabs"] if not tab['is_focused']]
+    tabs_and_projects = tabs[:]
     projects = []
 
     for dir in opts.dirs:
@@ -103,33 +50,27 @@ def load_main(args, opts):
 
     bin_path = os.getenv("BIN_PATH", "")
 
-    default_prompt = "🐈project"
-    flags = []
-    for org in opts.orgs:
-        flags.append(f"--org {org}")
-    for user in opts.users:
-        flags.append(f"--user {user}")
+    default_prompt = "🐈tabs> "
     # NOTE: Can't use ' char within any of the binds
     binds = [
-        f"ctrl-r:change-prompt(🐈remote> )+reload({bin_path}python3 ~/.config/kitty/meow/get_all_repos.py {' '.join(flags)})",
-        'ctrl-t:change-prompt(🐈tabs> )+reload(printf "{0}")'.format("\n".join(tabs)),
-        'alt-p:change-prompt(🐈projects> )+reload(printf "{0}")'.format(
+        'ctrl-p:change-prompt(🐈projects> )+reload(printf "{0}")'.format(
             "\n".join(projects)
         ),
-        'alt-l:change-prompt({0}> )+reload(printf "{1}")'.format(
-            default_prompt, "\n".join(tabs_and_projects)
+        'ctrl-o:change-prompt({0})+reload(printf "{1}")'.format(
+            default_prompt, "\n".join(tabs)
         ),
+        'ctrl-e:change-prompt(🐈> )+reload(printf "{0}")'.format("\n".join(tabs_and_projects))
     ]
     args = [
         f"{bin_path}fzf",
         "--multi",
         "--reverse",
-        "--header=ctrl-r: remote | alt-p: project | ctrl-t: tabs | alt-l: tabs&projects",
+        "--header=ctrl-p: project | ctrl-e: tabs | ctrl-o: tabs&projects",
         f"--prompt={default_prompt}> ",
         f"--bind={','.join(binds)}",
     ]
     p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out = p.communicate(input="\n".join(tabs_and_projects).encode())[0]
+    out = p.communicate(input="\n".join(tabs).encode())[0]
     output = out.decode().strip()
 
     # from kittens.tui.loop import debug
@@ -202,21 +143,12 @@ def load_handler(args: List[str], answer: str, target_window_id: int, boss: Boss
         load_project(boss, path, dir)
 
 
-def main(args: List[str]) -> str:
+def main(args: List[str]) -> list[str]:
     opts = parser.parse_args(args[1:])
-
-    if opts.command == "load":
-        return load_main(args, opts)
-    elif opts.command == "new":
-        return new_main(args, opts)
+    return load_main(args, opts)
 
 
 def handle_result(
     args: List[str], answer: str, target_window_id: int, boss: Boss
 ) -> None:
-    opts = parser.parse_args(args[1:])
-
-    if opts.command == "load":
-        return load_handler(args, answer, target_window_id, boss)
-    elif opts.command == "new":
-        return new_handler(args, answer, target_window_id, boss)
+    return load_handler(args, answer, target_window_id, boss)
